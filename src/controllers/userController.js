cb1fa7d2ea90b1232ba8b5cf567c1158fe3c5b95
Tejush Singh <tejush1998@gmail.com>
2022-10-10 18:35:21 +0530

    userValidator

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 const userModel = require("../models/userModel")
+const userVal = require("../validators/userValidator")
 
 const createUser = async function(req,res){
     try{
@@ -14,4 +15,84 @@ const createUser = async function(req,res){
 
 }
 
+const updateUser = async function(req,res){
+    try{
+
+        let data = req.body
+        let userId = req.params.userId
+        if(!Object.keys(data).length)
+        return res.status(400).send({status:false,message:"Send data in body"})
+
+        if(data.fname!=undefined)
+        {
+            if(userVal.isValidName(data.fname))
+            return res.status(400).send({status:false,message:"fname is invalid"})
+        }
+        if(data.lname!=undefined)
+        {
+            if(userVal.isValidName(data.lname))
+            return res.status(400).send({status:false,message:"lname is invalid"})
+        }
+        if(data.email!=undefined)
+        {
+            if(userVal.isValidEmail(data.email))
+            return res.status(400).send({status:false,message:"email is invalid"})
+        }
+        if(data.profileImage!=undefined)
+        {
+            if(!userVal.isValids3(data.profileImage))
+            return res.status(400).send({status:false,message:"s3 url is invalid"})
+        }
+        if(data.phone!=undefined)
+        {
+            if(userVal.isValidMobile(data.phone))
+            return res.status(400).send({status:false,message:"phone is invalid"})
+        }
+        if(data.password!=undefined)
+        {
+            if(userVal.isPassword(data.password))
+            return res.status(400).send({status:false,message:"password is invalid"})
+            data.password = bcrypt.hash(data.password, saltRounds, function(err, hash) {
+                return res.status(400).send({status:false,message:err})
+            });
+        }
+        if(data.address!=undefined)
+        {
+            if(data.address.shipping!=undefined)
+            {
+                if(data.address.shipping.street!=undefined)
+                {if(userVal.isValidName(data.address.shipping.street))
+                return res.status(400).send({status:false,message:"shipping street is invalid"})
+                }if(data.address.shipping.city!=undefined)
+                {if(userVal.isValidName(data.address.shipping.city))
+                return res.status(400).send({status:false,message:"shipping city is invalid"})
+                }if(data.address.shipping.pincode!=undefined)
+                {if(userVal.isValidPincode(data.address.shipping.pincode))
+                return res.status(400).send({status:false,message:"shipping pincode is invalid"})
+                }
+            }
+            if(data.address.billing!=undefined)
+            {
+                if(data.address.billing.street!=undefined)
+                {if(userVal.isValidName(data.address.billing.street))
+                return res.status(400).send({status:false,message:"billing street is invalid"})
+                }if(data.address.billing.city!=undefined)
+                {if(userVal.isValidName(data.address.billing.city))
+                return res.status(400).send({status:false,message:"billing city is invalid"})
+                }if(data.address.billing.pincode!=undefined)
+                {if(userVal.isValidPincode(data.address.billing.pincode))
+                return res.status(400).send({status:false,message:"billing pincode is invalid"})
+                }
+            }
+        }
+        let createUser = userModel.findOneAndUpdate({_id:userId},data,{new:true})
+        return res.status(200).send({status:false,data:createUser})
+
+    }
+    catch(error){
+        res.status(500).send({msg:error.message})
+    }
+}
+
 module.exports.createUser = createUser
+module.exports.updateUser = updateUser
---
+++ src/validators/userValidator.js
@@ -0,0 +1,147 @@
+
+
+function whitespace(str) {
+    return str.trim().indexOf(" ") >= 0
+}
+function stringContainsNumber(_string) {
+    return /\d/.test(_string);
+}
+
+function isEmail(emailAdress) {
+    let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    // w use for char * use for breakpoint $ for end
+    return regex.test(emailAdress)       
+}
+
+function isValids3(s3Url){
+    let regex=/(s3-|s3\.)?(.*)\.amazonaws\.com/
+    return regex.test(s3Url)
+}
+
+function isValidId(Id){
+    let regex = /^[0-9a-fA-F]{24}$/
+    return regex.test(Id)
+}
+
+
+function isPhoneNumber(number){
+    let regex = /^\d{10}$/;
+    return regex.test(number)
+}
+
+
+function isPincode(pin){
+    let regex = /^\d{6}$/;
+    return regex.test(pin)
+}
+
+const isValidate= function (value) {
+    if (typeof value === "undefined" || value === null) return false;
+    if (typeof value === "string" && value.trim().length > 0) return true;
+    return false;
+  };
+
+  const isValidateNum= function (value) {
+    if (typeof value === "undefined" || value === null) return false;
+    if (typeof value === "number") return true;
+    return false;
+  };
+
+
+
+
+const isValidName = function(name){
+    try{
+        if(!isValidate(name)){
+            return "Name should be given and type string"
+        }
+        if(stringContainsNumber(name)){
+            return "name should only contain letters"
+        }
+
+    }
+    catch(error){
+        return error.message
+
+    }
+}
+
+const isValidMobile = function (mobile) {
+    try {
+        if (!isValidate(mobile)) {
+            return "mobile number should be given and type string! "
+        }
+        if (whitespace(mobile)) {
+            return "Make sure mobile  number should not have space ! " 
+        }
+
+        let phone = isPhoneNumber(mobile)
+
+        if (phone == false) {
+            return "Please provide valid phone Number !" 
+        }
+        }
+        catch (error) {
+            return error.message
+        }
+}
+
+    
+const isValidEmail = function (email) {
+    try {
+        if (!isValidate(email)) {
+            return "email should be given and type string! "
+        }
+        if (whitespace(email)) {
+            return "Make sure email should not have any  space ! " 
+        }
+        let EmailId = isEmail(email)
+        if (EmailId == false) {
+            return "Please provide valid email address !" 
+        }
+    }
+    catch (error) {
+        return error.message
+    }
+}
+const isPassword = function (password) {
+    try {
+        if (!isValidate(password)) {
+            return "Passwords should be given and type string! "
+        }
+        if (whitespace(password)) {
+            return "Make sure email should not have any  space ! " 
+        }
+        if(password.length>15 || password.length<8)
+            return "Password length should be between 8 and 15 characters"
+       
+    }
+    catch (error) {
+        return error.message
+    }
+}
+
+
+const isValidPincode = function (pincode) {
+    try {
+        if (!isValidateNum(pincode)) {
+            return "pincode  should be given and type string! "
+        }
+        if (whitespace(pincode)) {
+            return "Make sure pincode  should not have space ! " 
+        }
+
+        let Pincode = isPincode(pincode)
+        if (Pincode == false) {
+            return "Please provide valid Pincode !" 
+        }
+
+    }
+    catch (error) {
+        return error.message
+    }
+}
+
+
+
+module.exports = {isValidId,isValidName,isValidMobile,isValidEmail,isValids3,isValidPincode,isPassword}
