7d6476183c058f23b574a8f88fb9924bddbfd686
laptop-mb <bansalmonika143@gmail.com>
2022-10-10 16:35:47 +0530

    strating

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const route = require('../src/route/route');
+const mongoose = require('mongoose');
+const app = express();
+
+app.use(bodyParser.json());
+
+mongoose.connect("mongodb+srv://bansalmonika123:mona@cluster0.4rsiaju.mongodb.net/group13Database", {
+    useNewUrlParser: true
+})
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
+
+app.use('/', route);
+
+app.all("/*", function (req, res) {
+    res.status(404).send({ status: false, message: "Kindly give correct information in path param ! UNDERSTAND" });
+});
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
