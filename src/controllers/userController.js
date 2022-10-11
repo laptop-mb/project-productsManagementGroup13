593d6184732982021d6e5d72b4f7f83bc15031fb
laptop-mb <bansalmonika143@gmail.com>
2022-10-11 11:49:52 +0530

    gh

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,9 +1,82 @@
 const userModel = require("../models/userModel")
 const userVal = require("../validators/userValidator")
+const bcrypt = require("bcrypt")
 
 const createUser = async function(req,res){
     try{
         let data = req.body
+        // let {fname, lname,email,profileImage, phone,password, address } = data
+
+        if(!Object.keys(data).length)
+        return res.status(400).send({status:false,message:"Send data in body"})
+
+        if(data.fname!=undefined)
+        {
+            if(userVal.isValidName(data.fname))
+            return res.status(400).send({status:false,message:"fname is invalid"})
+        }
+
+        if(data.lname!=undefined)
+        {
+            if(userVal.isValidName(data.lname))
+            return res.status(400).send({status:false,message:"lname is invalid"})
+        }
+        if(data.email!=undefined)
+        {
+            if(userVal.isValidEmail(data.email))
+            return res.status(400).send({status:false,message:"email is invalid"})
+        }
+        if(data.profileImage!=undefined)
+        {
+            if(!userVal.isValids3(data.profileImage))
+            return res.status(400).send({status:false,message:"s3 url is invalid"})
+        }
+        if(data.phone!=undefined)
+        {
+            if(userVal.isValidMobile(data.phone))
+            return res.status(400).send({status:false,message:"phone is invalid"})
+        }
+        if(data.password!=undefined)
+        {
+            if(userVal.isPassword(data.password))
+            return res.status(400).send({status:false,message:"password is invalid"})
+            data.password = bcrypt.hash(data.password, 10, function(err, hash) {
+                return res.status(400).send({status:false,message:err})
+            });
+        }
+        if(data.address!=undefined)
+        {data.address = JSON.parse(data.address)
+            if(data.address.shipping!=undefined)
+            {
+                if(data.address.shipping.street!=undefined)
+                {if(userVal.isValidName(data.address.shipping.street))
+                return res.status(400).send({status:false,message:"shipping street is invalid"})
+                }if(data.address.shipping.city!=undefined)
+                {if(userVal.isValidName(data.address.shipping.city))
+                return res.status(400).send({status:false,message:"shipping city is invalid"})
+                }if(data.address.shipping.pincode!=undefined)
+                {if(userVal.isValidPincode(data.address.shipping.pincode))
+                return res.status(400).send({status:false,message:"shipping pincode is invalid"})
+                }
+            }
+            if(data.address.billing!=undefined)
+            {
+                if(data.address.billing.street!=undefined)
+                {if(userVal.isValidName(data.address.billing.street))
+                return res.status(400).send({status:false,message:"billing street is invalid"})
+                }if(data.address.billing.city!=undefined)
+                {if(userVal.isValidName(data.address.billing.city))
+                return res.status(400).send({status:false,message:"billing city is invalid"})
+                }if(data.address.billing.pincode!=undefined)
+                {if(userVal.isValidPincode(data.address.billing.pincode))
+                return res.status(400).send({status:false,message:"billing pincode is invalid"})
+                }
+            }
+        }
+
+        let savedData = await userModel.create(data)
+        res.status(201).send({  "status": true,"message": "User created successfully",data: savedData})
+ 
         
 
     }
@@ -91,6 +164,28 @@ const updateUser = async function(req,res){
     }
     catch(error){
         res.status(500).send({msg:error.message})
+    }
+}
+
+
+const loginUser = async function (req, res) {
+    try {
+        let loginData = req.body
+        let { email, password } = loginData
+ 
+        let user = await userModel.findOne({ email: email });
+        if (!user) {
+            return res.status(404).send({ status: false, message: "Email Not found" });
+        }
+    
+        const validPassword = await bcrypt.compare(password, user.password)
+        if (!validPassword) {
+            return res.status(400).send({ status: false, message: "wrong password" })
+        }
+
+        return res.status(200).send({ status: true, message: "Success", data: { userId: user._id, token: token } })
+    } catch (err) {
+        return res.status(500).send({ status: false, message: err.message })
     }
 }
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,6 +3,9 @@ const  router = express.Router()
 const userController = require("../controllers/userController")
 
 
+router.post("/register", userController.createUser)
+
+
 
 
 module.exports = router
